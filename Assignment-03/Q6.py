e9daf3be205a8ab73eb65e3d7b5d17abd178e7cf
shivamsahaniii <shivamsahaniii17@gmail.com>
2025-12-29 22:12:22 +0530

    A3 Q 6 and 7

---
+++ Assignment-03/Q6.py
@@ -0,0 +1,13 @@
+# Given a list of words:
+# words = ["apple", "banana", "kiwi", "cherry", "mango"]
+# Create a dictionary that maps each word to its length.
+# Example:
+# {"apple": 5, "banana": 6, "kiwi": 4, ...}
+
+words = ["apple", "banana", "kiwi", "cherry", "mango"]
+
+dicWord = {}
+for word in words:
+    dicWord[word] = len(word)
+
+print(f"{words} \nwords after converting them into dictionary \n{dicWord}")
---
+++ Assignment-03/Q7.py
@@ -0,0 +1,9 @@
+# Write a program that takes a string from the user and prints the number of spaces in the string.
+
+sentence = input("Enter the sentence: ")
+count = 0
+for word in sentence:
+    if (word == " "):
+        count += 1
+
+print(f"the sentence ... {sentence} ... has {count} spaces..")
